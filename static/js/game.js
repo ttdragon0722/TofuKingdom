6702769372ee03185b9e698197d1352af3cfdf92
ttdragon <a0909956502@gmail.com>
2023-04-22 18:27:54 +0800

    noflask update

--- static/js/game.js
+++ static/js/game.js
@@ -130,9 +130,8 @@ $(document).ready(function () {
         $(".play-func").append(
             `
             <div class="play-end">
-            
-            <a href="/" id="back"><i class="fa-solid fa-house"></i></a>
-            <a href="/play" id="startBtn">Replay</a>
+            <a href="/index.html" id="back"><i class="fa-solid fa-house"></i></a>
+            <a href="/play.html" id="startBtn">Replay</a>
             </div>`
         );
 
